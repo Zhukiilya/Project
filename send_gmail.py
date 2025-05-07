dd0a2e289a3405e954bf6a0dfa0fbae00daaac47
Zhukiilya <ilyazhuchara@gmail.com>
2025-05-07 17:07:36 +0300

    Update and rename send_RSA_key.py to send_gmail.py

---
+++ send_gmail.py
@@ -0,0 +1,20 @@
+def send_RSA(email_address, password, text, reciever_email):
+    import smtplib
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587  
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    message = MIMEMultipart()
+    message["From"] = email_address
+    message["To"] = reciever_email
+    message.attach(MIMEText(text, "plain"))
+    try:
+       server = smtplib.SMTP(smtp_server, smtp_port)
+       server.starttls()  
+       server.login(email_address, password)
+       server.sendmail(email_address, reciever_email, message.as_string())
+       print("Письмо отправлено!")
+    except:
+       print("Ошибка")
+    finally:
+       server.quit()
