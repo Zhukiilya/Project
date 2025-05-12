16f5d934368fd9b6eccf6da7659701de5ec2365d
Zhukiilya <ilyazhuchara@gmail.com>
2025-05-13 02:51:04 +0300

    Update file_encrypt.py

--- file_encrypt.py
+++ file_encrypt.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.backends import default_backend
-# 0публичные и приватные ключи RSA для цифровой подписи, их обмен
-from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+# 0 публичные и приватные ключи RSA для цифровой подписи, их обмен
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 private_rsa_key_a = rsa.generate_private_key(
     public_exponent=65537,
     key_size=4096,  # Длина модуля n = 4096 бит
@@ -8,6 +9,9 @@ private_rsa_key_a = rsa.generate_private_key(
 )
 public_rsa_key_a = private_key.public_key()
 n_ae = public_rsa_key_a.public_numbers().n
+
+n_ae_bytes = n_ae.to_bytes((n.bit_length() + 7) // 8, byteorder="big")
+S_a = private_rsa_key_a.sign(n_ae_bytes, padding=padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=32), algorithm=hashes.SHA256())
 #
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives import serialization
@@ -20,9 +24,7 @@ d_b = X25519PrivateKey.generate()
 P_ax = d_a.public_key()
 P_bx = d_b.public_key()
 
-
-
-# #Сериализация ключей в стандартный формат (32-байтные x-координаты)
+#Сериализация ключей в стандартный формат (32-байтные x-координаты)
 public_key_a_bytes = public_key_a.public_bytes(
     encoding=serialization.Encoding.Raw,
     format=serialization.PublicFormat.Raw
